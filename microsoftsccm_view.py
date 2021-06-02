c108023fe1e7718f917e047e66236ebb365f0f0e
Mishal Patel <mishalp@splunk.com>
2021-06-02 22:42:49 +0000

    PAPP-17259 : Microsoft SCCM 2to3 conversion and JSON changes

--- microsoftsccm_view.py
+++ microsoftsccm_view.py
@@ -1,14 +1,9 @@
 # --
 # File: microsoftsccm_view.py
+# Copyright (c) 2017-2021 Splunk Inc.
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
 #
-# Copyright (c) Phantom Cyber Corporation, 2017-2018
-#
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber Corporation.
 #
 # --
 
